c9258d73b27959953a0d19a5d1aad1470c5923da
facundoLozano0 <facundo@gmai.com>
2022-12-01 16:39:18 -0300

    tarea2

---
+++ tarea2.js
@@ -0,0 +1,13 @@
+const yo= [Nombre="facundo",edad=21,desarrollador=true,fechaNacimiento=new Date("september 01 2001"), libroFav=libro];
+const libro={
+    Titulo:"elon musk",
+    autor:"elon musk",
+    fecha:new Date("sep 2000")
+
+};
+console.log(yo[1])
+
+
+ 
+
+ 
